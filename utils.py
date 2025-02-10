c2e8594e7424edea1582b3b6028ea02eed34406c
Ari <ari.stropeni01@gmail.com>
2025-02-10 20:35:03 +0100

    final tests breast

--- utils.py
+++ utils.py
@@ -134,7 +134,7 @@ def split_dataset(image_and_mask_list):
     train_pairs, val_test_pairs = train_test_split(image_and_mask_list, test_size=0.4, random_state=42)
 
     #split validation and test set
-    val_pairs, test_pairs = train_test_split(val_test_pairs, test_size=0.5, random_state=42)
+    val_pairs, test_pairs = train_test_split(val_test_pairs, test_size=1/2, random_state=42)
 
     return train_pairs, val_pairs, test_pairs
 
